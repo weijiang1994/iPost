d52d91fffa8d4d06f46ff1e29436748d9431c396
jiangwei1 <jiangwei1@kylinos.com>
2021-07-17 17:39:22 +0800

    feature: 主页面初始化

--- controller/main_view.py
+++ controller/main_view.py
@@ -7,3 +7,27 @@
 @Desc    : main_view
 @Software: PyCharm
 """
+from PyQt5.QtWidgets import QMainWindow
+from ui.main_window import Ui_MainWindow
+from controller.workspace_frame import WorkspaceFrame
+from utils.common import read_qss, basedir
+
+
+class MainView(Ui_MainWindow, QMainWindow):
+    def __init__(self):
+        super(MainView, self).__init__()
+        self.setupUi(self)
+        self.label.setVisible(False)
+        self.gridLayout.addWidget(WorkspaceFrame())
+        self.setStyleSheet(read_qss(basedir + '/resources/base.qss'))
+
+
+if __name__ == '__main__':
+    import sys
+    from PyQt5.QtWidgets import QApplication
+
+    app = QApplication(sys.argv)
+    win = MainView()
+    win.setWindowTitle('iPost Version 1.0.14 Beta')
+    win.show()
+    app.exec_()
--- controller/workspace_frame.py
+++ controller/workspace_frame.py
@@ -7,3 +7,15 @@
 @Desc    : workspace_frame
 @Software: PyCharm
 """
+from ui.workspace_view import Ui_Form
+from PyQt5.QtWidgets import QWidget
+from utils.common import read_qss, basedir
+
+
+class WorkspaceFrame(Ui_Form, QWidget):
+    def __init__(self):
+        super(WorkspaceFrame, self).__init__()
+        self.setupUi(self)
+        self.setStyleSheet(read_qss(basedir + '/resources/base.qss'))
+        self.workspace_listWidget.setCurrentRow(0)
+        self.workspace_stackedWidget.setAutoFillBackground(True)
