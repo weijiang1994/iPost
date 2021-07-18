7a3e2c1cb196e7bbf1d5ea101c58f9b71ed2b9df
weijiang1994 <804022023@qq.com>
2021-07-18 20:15:31 +0800

    feature: 完成的动态添加tab功能

--- controller/workspace_frame.py
+++ controller/workspace_frame.py
@@ -8,7 +8,7 @@
 @Software: PyCharm
 """
 from ui.workspace_view import Ui_Form
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QWidget, QMessageBox
 from utils.common import read_qss, basedir
 
 
@@ -17,5 +17,22 @@ class WorkspaceFrame(Ui_Form, QWidget):
         super(WorkspaceFrame, self).__init__()
         self.setupUi(self)
         self.setStyleSheet(read_qss(basedir + '/resources/base.qss'))
+        self.init_ui()
+        self.init_slot()
+
+    def init_ui(self):
         self.workspace_listWidget.setCurrentRow(0)
         self.workspace_stackedWidget.setAutoFillBackground(True)
+        self.tabWidget.removeTab(1)
+        self.tabWidget.removeTab(0)
+        self.tabWidget.setTabsClosable(True)
+
+    def init_slot(self):
+        self.new_pushButton.clicked.connect(self.new_request)
+
+    def new_request(self):
+        if self.tabWidget.count() > 2:
+            return
+        widget = QWidget()
+        self.tabWidget.addTab(widget, 'Test Request')
+        self.tabWidget.setCurrentWidget(widget)
