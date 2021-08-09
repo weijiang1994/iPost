e5d1678dfd43cbe822ffa958e005b4c2e9db45c2
weijiang1994 <804022023@qq.com>
2021-08-09 15:16:48 +0800

    fix: 删除多余代码

--- controller/component/table_view.py
+++ controller/component/table_view.py
@@ -24,49 +24,6 @@ completer.setFilterMode(Qt.MatchContains)
 completer.setCompletionMode(QCompleter.PopupCompletion)
 
 
-class CheckBoxHeader(QHeaderView):
-    clicked = pyqtSignal(bool)
-
-    _x_offset = 3
-    _y_offset = 0
-    _width = 20
-    _height = 20
-
-    def __init__(self, orientation=Qt.Horizontal, parent=None):
-        super(CheckBoxHeader, self).__init__(orientation, parent)
-        self.isOn = False
-
-    def paintSection(self, painter, rect, logicalIndex):
-        painter.save()
-        super(CheckBoxHeader, self).paintSection(painter, rect, logicalIndex)
-        painter.restore()
-
-        self._y_offset = int((rect.height() - self._width) / 2.)
-
-        if logicalIndex == 0:
-            option = QStyleOptionButton()
-            option.rect = QRect(rect.x() + self._x_offset, rect.y() + self._y_offset, self._width, self._height)
-            option.state = QStyle.State_Enabled | QStyle.State_Active
-            if self.isOn:
-                option.state |= QStyle.State_On
-            else:
-                option.state |= QStyle.State_Off
-            self.style().drawControl(QStyle.CE_CheckBox, option, painter)
-
-    def mousePressEvent(self, event):
-        index = self.logicalIndexAt(event.pos())
-        if 0 == index:
-            x = self.sectionPosition(index)
-            if x + self._x_offset < event.pos().x() < x + self._x_offset + self._width and self._y_offset < event.pos().y() < self._y_offset + self._height:
-                if self.isOn:
-                    self.isOn = False
-                else:
-                    self.isOn = True
-                self.clicked.emit(self.isOn)
-                self.update()
-        super(CheckBoxHeader, self).mousePressEvent(event)
-
-
 class TableWidget(QTableWidget):
     def __init__(self):
         super(TableWidget, self).__init__()
