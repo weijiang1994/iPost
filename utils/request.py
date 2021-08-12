66d54f77d2dbb61db7c4c6cf94f9786f7209f5b5
weijiang1994 <804022023@qq.com>
2021-08-12 18:56:58 +0800

    modify: 修改为request发送请求

--- utils/request.py
+++ utils/request.py
@@ -25,27 +25,31 @@ class RequestSession(Session):
     def send_request(self, method='GET', **kwargs):
         self._res = None
         try:
-            if method.upper() == 'GET':
-                self._res = self.get(self._url, **kwargs)
-            elif method.upper() == 'PUT':
-                self._res = self.put(self._url, **kwargs)
-            elif method.upper() == 'DELETE':
-                self._res = self.delete(self._url, **kwargs)
-            elif method.upper() == 'POST':
-                self._res = self.post(self._url, **kwargs)
+            # if method.upper() == 'GET':
+            #     self._res = self.get(self._url, **kwargs)
+            # elif method.upper() == 'PUT':
+            #     self._res = self.put(self._url, **kwargs)
+            # elif method.upper() == 'DELETE':
+            #     self._res = self.delete(self._url, **kwargs)
+            # elif method.upper() == 'POST':
+            #     self._res = self.post(self._url, **kwargs)
+            self._res = self.request(url=self._url, method=method, **kwargs)
             return {'result': True, 'response': self._res, 'error_msg': ''}
         except Exception:
             return {'result': False, 'response': self._res, 'error_msg': str(traceback.format_exc())}
 
 
-# a = {'headers': {'Access-Username': 'jiangwei1994', 'Access-Token': 'mMS2Dy7nGwVQt8649XU0rZ5gRbNuca3W'}, 'timeout': (None, 20)}
-# rs = RequestSession(url='http://127.0.0.1:8008/check_activation?z=213131&f=2314214412')
-# res = rs.send_request(method='get', **a)
-#
-# if res.get('result'):
-#     print(res.get('response').status_code)
-#     print(res.get('response').headers)
-#     print(res.get('response').cookies)
-#     print(res.get('response').json())
-# else:
-#     print(res.get('error_msg'))
+if __name__ == '__main__':
+
+    a = {'headers': {'Access-Username': 'jiangwei1994', 'Access-Token': 'mMS2Dy7nGwVQt8649XU0rZ5gRbNuca3W'},
+         'timeout': (None, 20)}
+    rs = RequestSession(url='http://127.0.0.1:8008/check_activation?z=213131&f=2314214412')
+    res = rs.send_request(method='get', **a)
+
+    if res.get('result'):
+        print(res.get('response').status_code)
+        print(res.get('response').headers)
+        print(res.get('response').cookies)
+        print(res.get('response').json())
+    else:
+        print(res.get('error_msg'))
